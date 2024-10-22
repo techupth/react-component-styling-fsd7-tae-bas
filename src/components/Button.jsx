c7f9800a6e6c02fae6e5966cd138373c85d77775
Sappakit <pipitwattana.s@gmail.com>
2024-10-22 16:13:07 +0700

    chore: remove unused code on ex1

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,13 +3,6 @@
 import { css } from "@emotion/react";
 
 function Button({ type }) {
-  const customColor = {
-    Primary: "#074EE8",
-    Secondary: "#07A4E8",
-    Error: "#DE5753",
-    Success: "#26B795",
-  };
-
   return (
     <button
       css={css`
